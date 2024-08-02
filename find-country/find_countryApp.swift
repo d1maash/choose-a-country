0217ac0ae579b6c78dd1478dca6a8cc5de9acdfe
Dimash <dimashaijarikov@gmail.com>
2024-08-02 23:56:28 +0500

    Initial Commit

---
+++ find-country/find_countryApp.swift
@@ -0,0 +1,17 @@
+//
+//  find_countryApp.swift
+//  find-country
+//
+//  Created by Динмуханбет Айжарыков on 02.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct find_countryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
